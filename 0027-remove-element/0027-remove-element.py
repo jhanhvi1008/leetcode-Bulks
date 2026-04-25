e701aaff4de04f54c7c27d30a1df405b72a46086
Jhanhvi Tripathi <109922282+jhanhvi1008@users.noreply.github.com>
2026-04-26 01:07:23 +0530

    Time: 0 ms (100%), Space: 19.3 MB (21.23%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.py
@@ -0,0 +1,11 @@
+class Solution:
+    def removeElement(self, nums, val):
+        k = 0  # pointer for valid elements
+
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[k] = nums[i]
+                k += 1
+
+        return k
+        
