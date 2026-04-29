7a542637bb8a0cf9c15e77ed7e69052d9d5e353b
Jhanhvi Tripathi <109922282+jhanhvi1008@users.noreply.github.com>
2026-04-30 01:13:25 +0530

    Time: 0 ms (100%), Space: 19.4 MB (87.23%) - LeetHub

---
+++ 0046-permutations/0046-permutations.py
@@ -0,0 +1,17 @@
+class Solution:
+    def permute(self, nums):
+        result = []
+        
+        def backtrack(path, remaining):
+            # If no elements left, we found a permutation
+            if not remaining:
+                result.append(path)
+                return
+            
+            for i in range(len(remaining)):
+                # Choose element
+                backtrack(path + [remaining[i]], 
+                          remaining[:i] + remaining[i+1:])
+        
+        backtrack([], nums)
+        return result
